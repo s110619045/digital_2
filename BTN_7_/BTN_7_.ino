a2e931ed76a8dd1dfd92ca85d8d318e99db87dc0
s110619045 <49371664+s110619045@users.noreply.github.com>
2019-05-15 12:01:03 +0800

    Add files via upload

---
+++ BTN_7_/BTN_7_.ino
@@ -0,0 +1,60 @@
+byte seven_seg_digits[10][7] = { { 1,1,1,1,1,1,0 },  // = 0
+                                 { 0,1,1,0,0,0,0 },  // = 1
+                                 { 1,1,0,1,1,0,1 },  // = 2
+                                 { 1,1,1,1,0,0,1 },  // = 3
+                                 { 0,1,1,0,0,1,1 },  // = 4
+                                 { 1,0,1,1,0,1,1 },  // = 5
+                                 { 1,0,1,1,1,1,1 },  // = 6
+                                 { 1,1,1,0,0,0,0 },  // = 7
+                                 { 1,1,1,1,1,1,1 },  // = 8
+                                 { 1,1,1,0,0,1,1 }   // = 9
+                             };
+                             
+int num = 0;
+
+const int inputBtn=12;
+int btnInput=0;
+
+
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(9600);
+  pinMode(2, OUTPUT);  
+  pinMode(3, OUTPUT);
+  pinMode(4, OUTPUT);
+  pinMode(5, OUTPUT);
+  pinMode(6, OUTPUT);
+  pinMode(7, OUTPUT);
+  pinMode(8, OUTPUT);
+  pinMode(9, OUTPUT);
+  digitalWrite(9, 0);  // 關閉小數點
+
+  pinMode(inputBtn,INPUT);
+  digitalWrite(inputBtn,HIGH);
+  
+}
+
+void sevenSegWrite(byte digit) {
+  byte pin = 2;
+  for (byte seg = 0; seg < 7; ++seg) {
+    digitalWrite(pin, seven_seg_digits[digit][seg]);
+    ++pin;
+  }
+}
+
+void loop() {
+  btnInput=digitalRead(inputBtn);
+if(btnInput==LOW){
+ num=0;
+}
+// put your main code here, to run repeatedly:
+ sevenSegWrite(num); 
+ Serial.println(num);
+ delay(1000);
+ num++;
+ 
+ if(num>9){
+  num=0;
+ }
+ 
+}
